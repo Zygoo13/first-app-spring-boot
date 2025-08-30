b0b9b82eb941fa0704d2a6242a69a9c0dad017fa
Zygoo13 <AnhHQ.B23CN022@stu.ptit.edu.vn>
2025-08-31 05:42:12 +0700

    Update UserCreationRequest.java
    #2 test

--- src/main/java/com/zygoo132/first_app/dtos/requests/UserCreationRequest.java
+++ src/main/java/com/zygoo132/first_app/dtos/requests/UserCreationRequest.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 public class UserCreationRequest {
     @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     private String username;
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     private String password;
     private String firstName;
     private String lastName;
